26074d8e20f57e2217eaba2186f322149972b168
chirimoya-pudu <magnificentpudu@gmail.com>
2026-03-18 18:56:54 -0400

    Fix rust rate reduction and scale it based on pred progression

--- src/character.cpp
+++ src/character.cpp
@@ -4243,7 +4243,7 @@ void Character::apply_skill_boost()
 
 void Character::do_skill_rust()
 {
-    int rust_rate_tmp = rust_rate();
+    const int rust_rate_tmp = rust_rate();
     for( std::pair<const skill_id, SkillLevel> &pair : *_skills ) {
         const Skill &aSkill = *pair.first;
         SkillLevel &skill_level_obj = pair.second;
@@ -4273,15 +4273,20 @@ void Character::do_skill_rust()
                                      has_active_bionic( bio_memory );
         const int oldSkillLevel = skill_level_obj.level();
 
-        // if the current skill is a combat skill and PRED2/3/4 are active, reduce rust rate by 20%
-        if( aSkill.is_combat_skill() &&
-            ( has_trait_flag( trait_flag_PRED2 ) ||
-              has_trait_flag( trait_flag_PRED3 ) ||
-              has_trait_flag( trait_flag_PRED4 ) ) ) {
-            rust_rate_tmp = rust_rate_tmp * 0.8;
+        // default multiplier is 1; if PRED2/3/4 are enabled, reduce the multiplier accordingly
+        int rust_rate_multiplier = 1;
+
+        if ( aSkill.is_combat_skill() ) {
+            if ( has_trait_flag( trait_flag_PRED2 ) ) {
+                rust_rate_multiplier = 0.8;
+            } else if ( has_trait_flag( trait_flag_PRED3 ) ) {
+                rust_rate_multiplier = 0.6;
+            } else if ( has_trait_flag( trait_flag_PRED4 ) ) {
+                rust_rate_multiplier = 0.4;
+            }
         }
 
-        if( skill_level_obj.rust( charged_bio_mem, rust_rate_tmp ) ) {
+        if( skill_level_obj.rust( charged_bio_mem, rust_rate_tmp * rust_rate_multiplier ) ) {
             add_msg_if_player( m_warning,
                                _( "Your knowledge of %s begins to fade, but your memory banks retain it!" ), aSkill.name() );
             mod_power_level( -bio_memory->power_trigger );
