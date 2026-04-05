cbd44290ca8157b88239f46e28e9df3d224a786f
scarf <greenscarf005@gmail.com>
2026-04-06 01:09:30 +0900

    Update src/crafting.cpp

--- src/crafting.cpp
+++ src/crafting.cpp
@@ -1552,11 +1552,23 @@ std::vector<detached_ptr<item>> Character::consume_items( map &m,
 
     item_comp selected_comp = is.comp;
 
-    const tripoint &loc = origin;
     const bool by_charges = item::count_by_charges( selected_comp.type ) && selected_comp.count > 0;
     // Count given to use_amount/use_charges, changed by those functions!
     int real_count = ( selected_comp.count > 0 ) ? selected_comp.count * batch : std::abs(
-                         selected_comp.count );
+        selected_comp.count );
+
+    if( has_trait( trait_DEBUG_HS ) ) {
+        if( by_charges ) {
+            ret.push_back( item::spawn( selected_comp.type, calendar::start_of_cataclysm, real_count ) );
+        } else {
+            for( int i = 0; i < real_count; i++ ) {
+                ret.push_back( item::spawn( selected_comp.type ) );
+            }
+        }
+        return ret;
+    }
+
+    const tripoint &loc = origin;
     // First try to get everything from the map, than (remaining amount) from player
     if( is.use_from & usage_from::map ) {
         if( by_charges ) {
